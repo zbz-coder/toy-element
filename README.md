3b16f36d8c8c2270d8d2be15741679b2dbfdc02a
unknown <2168393741@qq.com>
2024-12-02 23:54:10 +0800

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 项目简介
+
+## 项目分包结构
+
+```bash
+- components # 组件目录
+- core # npm 包入口
+- docs # 文档目录
+- hooks # 组合式 API hooks 目录
+- play # 组件开发实验室
+- theme # 主题目录
+- utils # 工具函数目录
+```
---
+++ packages/components/Button/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: "ErButton",
+});
+</script>
+
+<template>
+  <button style="background-color: blue; color: red">this is a button</button>
+</template>
+
+<style lang="scss" scoped></style>
